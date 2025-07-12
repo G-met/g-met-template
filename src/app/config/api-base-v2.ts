ed07525235f8f17ea2767a017c7c774c28c0be60
andrestejada <aneduardotejada@gmail.com>
2025-07-11 22:42:59 -0500

    rename env var

--- src/app/config/api-base-v2.ts
+++ src/app/config/api-base-v2.ts
@@ -4,7 +4,7 @@ axios.defaults.withCredentials = true;
 
 const isServer = typeof window === "undefined";
 
-const baseURL = isServer ? process.env.API_DOMAIN : process.env.NEXT_API_DOMAIN;
+const baseURL = isServer ? process.env.API_DOMAIN : process.env.NEXT_PUBLIC_API_BASE_URL;
 console.log("Base URL:", baseURL);
 console.log("Is Server:", isServer);
 export const httpBaseV2 = axios.create({
