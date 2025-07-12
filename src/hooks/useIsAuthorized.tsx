2753f359f2460915a6c7495196052f7c8f00777e
andrestejada <aneduardotejada@gmail.com>
2025-07-11 23:12:56 -0500

    log

--- src/hooks/useIsAuthorized.tsx
+++ src/hooks/useIsAuthorized.tsx
@@ -4,6 +4,7 @@ import { useUser } from "@clerk/nextjs";
 
 export function useIsAuthorized(authorizedRoles: Role[]) {
   const { user } = useUser();
+  console.log("User role:", user?.publicMetadata.role);
   const currentRole = (user?.publicMetadata.role as Role) ?? Role.Consulta;
   return rolesGuard(authorizedRoles, currentRole);
 }
