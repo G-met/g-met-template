439efdb8bae9caf357b74b13213e791d7afd0bb3
andrestejada <aneduardotejada@gmail.com>
2025-07-11 23:06:51 -0500

    send token in client request

--- src/app/config/api-base-v2.ts
+++ src/app/config/api-base-v2.ts
@@ -4,9 +4,33 @@ axios.defaults.withCredentials = true;
 
 const isServer = typeof window === "undefined";
 
-const baseURL = isServer ? process.env.API_DOMAIN : process.env.NEXT_PUBLIC_API_BASE_URL;
+const baseURL = isServer
+  ? process.env.API_DOMAIN
+  : process.env.NEXT_PUBLIC_API_BASE_URL;
 console.log("Base URL:", baseURL);
 console.log("Is Server:", isServer);
 export const httpBaseV2 = axios.create({
   baseURL,
 });
+
+if (!isServer) {
+  httpBaseV2.interceptors.request.use(async (config) => {
+    const { Clerk } = await import("@clerk/clerk-js");
+
+    const clerk = new Clerk(
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ?? ""
+    );
+    try {
+      await clerk.load();
+      const token = await clerk?.session?.getToken();
+
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.warn("No se pudo obtener el token de Clerk", error);
+    }
+
+    return config;
+  });
+}
