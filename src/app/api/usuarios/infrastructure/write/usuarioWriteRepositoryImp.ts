22319cb5fa807856f0011ba2bdf2c8ac75c1c05d
andrestejada <aneduardotejada@gmail.com>
2025-08-04 21:09:28 -0500

    remove unnecesary code

--- src/app/api/usuarios/infrastructure/write/usuarioWriteRepositoryImp.ts
+++ src/app/api/usuarios/infrastructure/write/usuarioWriteRepositoryImp.ts
@@ -17,13 +17,11 @@ export class UsuarioWriteRepositoryImp implements UsuarioWriteRepository {
   async crearUsuarios(usuario: Usuario): Promise<void> {
     await prisma.usuario.create({
       data: {
-        usuario: usuario.usuario,
         nombre: usuario.nombre,
         apellido: usuario.apellido,
         cargo: usuario.cargo,
         rol: usuario.rol as Role,
         correo: usuario.correo,
-        password: usuario.password,
         clienteId: usuario.cliente.id,
       },
     });
