c5ee1f21dfa06d1bb050ccf421f4858d4d2a21ec
Andres Tejada <62909265+andrestejada@users.noreply.github.com>
2025-09-11 21:29:31 -0500

    Feature/patterns v2 (#106)
    * Refactor patron schema and update user/proveedor tables
    
    * Add patterns hook/service and fix user role mappings
    
    * feat(patrones): Refactor pattern creation form with shadcn/ui
    
    * feat: Implement paginated data table for patterns
    
    * Introduce DataTableV2 with improved loading and empty handling

--- src/app/dashboard/patrones/consultar/page.tsx
+++ src/app/dashboard/patrones/consultar/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useMemo } from "react";
 import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 import { useGetAllPatterns } from "../hook/usePattern";
@@ -8,6 +9,7 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import { DataTablePagination } from "@/components/data-table-pagination";
+import { DataTableV2 } from "@/components/data-table-v2";
 
 export default function ConsultarPatrones() {
   const { patterns, isLoading } = useGetAllPatterns();
@@ -18,10 +20,12 @@ export default function ConsultarPatrones() {
     getPaginationRowModel: getPaginationRowModel(),
   });
 
+  console.log(table);
+
   return (
     <div className="container mx-auto py-10">
       <h2 className="text-center mb-4 font-semibold">Consultar Patrones</h2>
-      <DataTable columns={columns} table={table} isLoading={isLoading} />
+      <DataTableV2 columns={columns} table={table} isLoading={isLoading} />
       <DataTablePagination table={table} />
     </div>
   );
--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -1,12 +1,12 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
   useReactTable,
   getPaginationRowModel,
-  Table as TanstackTable,
 } from "@tanstack/react-table";
 import {
   Table,
@@ -20,15 +20,23 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
-  table: TanstackTable<TData>;
-  isLoading: boolean;
+  data: TData[];
+  isLoading: boolean; // New prop to indicate loading state
 }
 
 export function DataTable<TData, TValue>({
   columns,
-  table,
+  data,
   isLoading,
 }: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  // Function to render skeleton rows
   const renderSkeletonRows = () => {
     return Array.from({ length: 5 }).map((_, index) => (
       <TableRow key={`skeleton-${index}`}>
@@ -42,29 +50,30 @@ export function DataTable<TData, TValue>({
   };
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <TableHead key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </TableHead>
-              ))}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {isLoading
-            ? renderSkeletonRows()
-            : table.getRowModel().rows?.length
-            ? table.getRowModel().rows.map((row) => (
+    <>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {isLoading ? (
+              renderSkeletonRows()
+            ) : table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
                 <TableRow
                   key={row.id}
                   data-state={row.getIsSelected() && "selected"}
@@ -79,18 +88,19 @@ export function DataTable<TData, TValue>({
                   ))}
                 </TableRow>
               ))
-            : !isLoading && (
-                <TableRow>
-                  <TableCell
-                    colSpan={columns.length}
-                    className="h-24 text-center"
-                  >
-                    No se encontraron resultados
-                  </TableCell>
-                </TableRow>
-              )}
-        </TableBody>
-      </Table>
-    </div>
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No se encontraron resultados
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   );
 }
