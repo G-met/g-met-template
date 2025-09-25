c73edc2a8c6f3aa920229f3f838f9c2a87173c6e
andrestejada <aneduardotejada@gmail.com>
2025-09-24 21:37:40 -0500

    refactor: remove TipoPatron related files and components

--- src/app/api/tipoPatron/repositorio/index.ts
+++
@@ -1,9 +0,0 @@
-import { TipoPatron } from "../dominio";
-import { CrearTipoPatronDto } from "../dtos/crear";
-
-export interface TipoPatronRespositorio {
-  creatTipoPatron: (
-    dto: CrearTipoPatronDto,
-    clienteId: string
-  ) => Promise<TipoPatron>;
-}
